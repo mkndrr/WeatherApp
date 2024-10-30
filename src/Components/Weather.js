52d4ed5e884d6f1c50e6783ffd3c473db13a6cc7
mkndrr <masha.kondratova0209@gmail.com>
2024-10-30 16:32:48 +0300

    add styles

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 const Weather = (props) => (
-    <div>
+    <div className="infoWeath">
         { props.city && 
             <div>
                 <p>Местоположение: {props.city}, {props.country}</p>
-                <p>Температура: {props.temp}</p>
-                <p>Давление: {props.pressure}</p> 
+                <p>Температура: {props.temp} °C</p>
+                <p>Давление: {props.pressure} мм.рт.ст.</p> 
                 <p>Заход солнца: {props.sunset}</p>
             </div>
         }
-        <p>{ props.error }</p>
+        <p className="error">{ props.error }</p>
     </div>
 );
 
