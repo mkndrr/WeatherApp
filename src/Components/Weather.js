241a812e6557cc3c9500519b790e043ec5798729
mkndrr <masha.kondratova0209@gmail.com>
2024-10-30 13:58:47 +0300

    code optimization

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,22 +1,17 @@
 import React from "react";
 
-class Weather extends React.Component {
-    render(){
-        return(
+const Weather = (props) => (
+    <div>
+        { props.city && 
             <div>
-                { this.props.city && //делаем так, чтобы вначале эти параграфы не отображались и выводились только тогда, когда вводим город
-                    <div>
-                        <p>Местоположение: {this.props.city}, {this.props.country}</p>
-                        <p>Температура: {this.props.temp}</p>
-                        <p>Давление: {this.props.pressure}</p> 
-                        <p>Заход солнца: {this.props.sunset}</p>
-                    </div>
-                }
-                <p>{ this.props.error }</p>
+                <p>Местоположение: {props.city}, {props.country}</p>
+                <p>Температура: {props.temp}</p>
+                <p>Давление: {props.pressure}</p> 
+                <p>Заход солнца: {props.sunset}</p>
             </div>
-        );
-
-    }
-}
+        }
+        <p>{ props.error }</p>
+    </div>
+);
 
 export default Weather;
